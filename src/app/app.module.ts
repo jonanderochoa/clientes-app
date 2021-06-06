7775342b86e69836b8f130ce7147d2b4cc64f468
Jon Ander Ochoa <jonanderochoa@gmail.com>
2021-06-06 22:50:23 +0200

    Componente cliente y su service
    Creacion del componente clientes que recibe los datos de un array y  muestra los datos en una tabla.
    Ademas se crea la clase ClienteService.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { ClienteService } from './clientes/cliente.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ClientesComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ClienteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/clientes/cliente.ts
@@ -0,0 +1,7 @@
+export class Cliente {
+    id: number | undefined;
+    nombre: string | undefined;
+    apellido: string | undefined;
+    email: string | undefined;
+    createAt: string | undefined;
+}

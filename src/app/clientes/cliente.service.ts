dbb29e416176ab6b21c90f658178ef1b1f1b6bbc
Jon Ander Ochoa <jonanderochoa@gmail.com>
2021-07-11 03:03:11 +0200

    Crud clientes
    Crud de clientes

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
-import { Observable, of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +9,27 @@ import { HttpClient } from '@angular/common/http';
 export class ClienteService {
 
   private urlEndPoint:string ='http://localhost:8080/clientes/';
+  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
+
   constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]>  {
     return this.http.get<Cliente[]>(this.urlEndPoint);
+  }
+
+  getCliente(id: number): Observable<Cliente> {
+    return this.http.get<Cliente>(`${this.urlEndPoint}${id}`)
+  }
+
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, { headers: this.httpHeaders })
+  }
+
+  update(cliente: Cliente): Observable<Cliente> {
+    return this.http.put<Cliente>(`${this.urlEndPoint}${cliente.id}`, cliente, { headers: this.httpHeaders })
+  }
+
+  delete(id: any): Observable<Cliente> {
+    return this.http.delete<Cliente>(`${this.urlEndPoint}${id}`, { headers: this.httpHeaders })
   }
 }
